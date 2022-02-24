91e5a0dbc07a89e9c717de57387e074df1f4d2f4
billdevmaster71 <billdevmaster@gmail.com>
2022-02-24 15:58:30 +0900

    fix: setting start_time

--- resources/views/backend/home/index.blade.php
+++ resources/views/backend/home/index.blade.php
@@ -164,7 +164,7 @@
                 $("#order_modal").modal("show");
                 if (time != null) {
                     $("#start_time").val(time)
-                } else {
+                } else if (uid != 0) {
                     var today = new Date();
                     var dd = String(today.getDate()).padStart(2, '0');
                     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
